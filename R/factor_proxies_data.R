ea33ce4f9b924d6e01fe3d9bb19c6510a8641e7e
leslie <meihaonan1015@gmail.com>
2022-06-06 20:31:18 +0800

    Release internel data and namespace

---
+++ R/factor_proxies_data.R
@@ -0,0 +1,17 @@
+#' Rra data on factor proxies
+#'
+#' 70 factor proxies, including
+#' the FF five factors, momentum factor,
+#' Pastor and Stambaugh (2003) liquidity factor, Hou,
+#' Xue, and Zhang (2015) ROE factor,
+#' and value-weighted decile spread portfolios of 62 CAPM anomalies.
+#'
+#' @docType data
+#'
+#' @usage data("70_factor_proxies_74-16")
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data("70_factor_proxies_74-16")
+
---
+++ R/industry_portfolios_data.R
@@ -0,0 +1,12 @@
+#' Rra data on industry portfolio returns
+#'
+#' 48 industry portfolios
+#'
+#' @docType data
+#'
+#' @usage data("48_ports_ret_74-16")
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data("48_ports_ret_74-16")
